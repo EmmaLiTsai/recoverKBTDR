fe79b427a20bb868476873f1b1d5cecbf9c8811f
emtsai <emmali.tsai@gmail.com>
2021-03-25 11:35:03 -0500

    Organizing / tidying files

--- r_scripts/Tsai_TimePeriods.R
+++ r_scripts/Tsai_TimePeriods.R
@@ -1,57 +1,17 @@
 ################################################################################
 # Author: EmmaLi Tsai
-# Topic: Time Periods, Depth, and Smoothing  
+# Topic: Full Trace Recovery 
 # Date: 2021-1-18
 ################################################################################
+# This script is broken up into different objectives: 
 # 
-#
-# This document builds off the arc transformation .R file to also include work
-# on the time periods, depth scale, centering, and spline smoothing. The final 
-# graph is the closest fully recovered trace that I have made so far ET (2/24)
-#
-#
-# The final output of this script produces a recovered trace, but some 
-# small issues still need to be fixed: 
-# 
-# TODO: Currently, I am working on reducing the "noise" at depth = 0, and 
-#       fixing the spline smoothing so that the smoothed line picks up when the 
-#       seal resurfaces for ~10 minutes during a bout of dives. This is the most 
-#       urgent task before I attempt the ones below. 
-#  
-#   UPDATE: somewhat resolved? Smoothing issues are resolved, but I am currently
-#       checking the new method for removing noise at depth = 0. See last 
-#       section of this document. ET 2/24
-# 
-# TODO: I need to double-check the depth transformation. The transformation 
-#       itself is easy, but I had to "center" the scan and I'm not confident 
-#       that I did it correctly. There is a software from NeruaScanner that can
-#       center the scan for me, but I want to see if I can do it in code. 
-# 
-#   UPDATE: I have centered the scan by using the timing dots in this update. 
-#       See code below. ET 2/15
-# 
-# TODO: some of the smoothed y_values seem to stray from the overall trend of
-#       the trace (i.e. picking up the other edge of the line). Maybe this could 
-#       be fixed by changing the spar value? 
-# 
-#   UPDATE: This issue is somewhat fixed by grouping the data by x value and 
-#       the spline smoothing function that I created that breaks up the data 
-#       into chunks and applies smoothing over smaller degrees of freedom. 
-#       However, a new challenge emerges when trying to identify when the seal 
-#       surfaces within a bout of dives-- see to-do task number 1. ET 2/15
-# 
-#   UPDATE: This has been resolved. I changed the image processing workflow to 
-#       skeletonize the trace, which greatly improved smoothing.  ET 2/24
-#         
-# TODO: check the output of this code with the castellini (1992) bulletin. The 
-#       max depth for this trace seems to align, but confirm other dive
-#       parameters to ensure the quality of these methods. 
-#
-#   UPDATE: removed depth transformation section for now, will come back to this 
-#       once I tackle the noise and smoothing issues above. ET 2/15
-#
-#   UPDATE: modified image processing by using "skeletonization" which helps the 
-#       smoothing functions capture more of the behavior at depth. 
+#   Removing the arc (row 74)
+#   Spline smoothing (row 98)
+#   Adding time dots (row 195)
+#   Transforming x-axis into time (row 277)
+#   Centering the scan (row 400)
+#   Adding dates and times (row 561)
+
 ################################################################################
 # Loading packages
 
@@ -90,10 +50,9 @@ START_TIME <- "1981:01:16 15:10:00"
 ################################################################################
 # Read data
 
-# this is data from an actual trace from 1981. 
+# working directory should be sample_data folder 
 
-# UPDATE -- changed image processing workflow, so the file names changed ET 2/24
-# see image processing methods in sample_data folder.
+# reading in trace data 
 trace <- read.csv("./skele_trace.csv", header = TRUE, 
                   stringsAsFactors = FALSE)
 
@@ -111,8 +70,7 @@ time_dots <- select(time_dots, -c("X.1"))
 names(time_dots) <- c("x_val", "Y")
 
 ################################################################################
-# Transforming the arc using the new approach
-# This code is from the Tsai_ArcFixing_NewApprach.R file 
+# Removing the Arc 
 ################################################################################
 
 # removing extra columns
@@ -366,9 +324,6 @@ trace$time_period <- flag_timeperiod(trace)
 
 # Okay, now that I have a flag for the different time periods, time to see if I  
 # can create a running time with the time scales I created above. 
-
-# I noticed issues with time in previous approach. Here, I am trying a new on  
-# 3/3/2021 ET
 
 # just grabbing the time points on the trace: 
 just_tp <- trace[which(trace$time_points_x!=0),]
