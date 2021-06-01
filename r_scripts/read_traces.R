f0aceb1e45197aecd9391515264f3ca449741cf7
emtsai <emmali.tsai@gmail.com>
2021-06-01 13:34:47 -0500

    minor formatting change

--- r_scripts/read_traces.R
+++ r_scripts/read_traces.R
@@ -9,7 +9,7 @@
 
 read_traces <- function(filepath = "../sample_data"){
   # listing the files 
-  trace_list <- list.files(path=filepath, pattern="*.csv", full.names=TRUE)
+  trace_list <- list.files(path = filepath, pattern = "*.csv", full.names = TRUE)
   # extracting the names of the files to read them in 
   names <- sub('\\.csv$', '', basename(trace_list))
   # removing the beginning of the file name so it works better for future 
@@ -20,6 +20,6 @@ read_traces <- function(filepath = "../sample_data"){
   # giving them appropriate names
   names(trace_files) <- names
   # reading them into the global environment... which seems dangerous...
-  invisible(lapply(names(trace_files), function(x) assign(x, trace_files[[x]], envir=.GlobalEnv)))
+  invisible(lapply(names(trace_files), function(x) assign(x, trace_files[[x]], envir = .GlobalEnv)))
 }
 
