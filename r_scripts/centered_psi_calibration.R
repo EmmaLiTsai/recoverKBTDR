83da7b54cb3ab7ac248174a55f5fc4230907d1a8
emtsai <emmali.tsai@gmail.com>
2021-06-09 09:43:00 -0500

    added function header

--- r_scripts/centered_psi_calibration.R
+++ r_scripts/centered_psi_calibration.R
@@ -1,3 +1,27 @@
+###############################################################################
+# Function: centered_psi_calibration(trace, psi_interval = c(100, 200, 400, 600, 800))
+# Author:   EmmaLi Tsai
+# Date:     6/09/21
+# 
+# This function takes the centered trace file to find the new centered values 
+# of the psi calibration intervals at the end of the record. This is supposed 
+# to be an improved method from the original, where the psi calibration 
+# intervals were measured on the scanned image in ImageJ before centering. 
+# Although the change in the position of the psi intervals is slight, I thought 
+# this should make future depth calculations more precise.  
+# 
+# Input: 
+# 
+#   - trace       : tidy trace data frame after centering, contains the x and y 
+#                   values of the trace.
+#
+#   - psi_intervals : a numeric vector of the psi intervals at the end of the 
+#                     record. Default is set to 100, 200, 400, 600, and 800 psi. 
+#   
+# Output: 
+#   - psi_calibration : data frame that contains the new psi positions after 
+#                       the trace had been centered
+###############################################################################
 centered_psi_calibration <- function(trace, psi_interval = c(100, 200, 400, 600, 800)){
   # grabbing the last chunk of data in the trace 
   start_row <- nrow(trace) - 2000
@@ -25,6 +49,7 @@ centered_psi_calibration <- function(trace, psi_interval = c(100, 200, 400, 600,
   # making psi data frame
   psi_calibration <- data.frame(psi_interval = psi_interval, 
                                 psi_position = final_psi)
+  # returning final data frame 
   return(psi_calibration)
 }
 
